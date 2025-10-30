131c70accb57ccdc668a86d5d6c371a26f220ebf
Erick G. Islas-Osuna <erickisos653@gmail.com>
2025-10-30 16:10:59 -0600

    feat: simplify codesnap function

--- codesnap.el
+++ codesnap.el
@@ -21,64 +21,23 @@
 
 (defgroup codesnap nil
   "Emacs integration for codesnap."
-  :group 'tools
+  :group  'tools
   :prefix "codesnap-")
 
 (defcustom codesnap-binary "codesnap"
   "Path to the codesnap binary."
-  :type 'string
+  :type  'string
   :group 'codesnap)
 
 (defcustom codesnap-log-buffer "*CodeSnap Messages*"
   "Name of the buffer used for logging codesnap operations."
-  :type 'string
+  :type  'string
   :group 'codesnap)
 
-(defun codesnap--get-file-extension ()
-  "Get the file extension for the current buffer."
-  (when (buffer-file-name)
-    (file-name-extension (buffer-file-name))))
-
-(defun codesnap--create-temp-file (content)
-  "Create a temporary file with CONTENT and appropriate extension."
-  (let* ((file-ext (codesnap--get-file-extension))
-         (temp-file (make-temp-file "codesnap-" nil (if file-ext (concat "." file-ext) ".txt"))))
-    (with-temp-file temp-file
-      (insert content))
-    temp-file))
-
-(defun codesnap--log-execution (temp-file content)
-  "Log the codesnap execution details."
-  (let ((log-buffer (get-buffer-create codesnap-log-buffer))
-        (command (format "%s --from-file %s --output clipboard --has-line-number" codesnap-binary temp-file))
-        (timestamp (format-time-string "%Y-%m-%d %H:%M:%S"))
-        (file-ext (codesnap--get-file-extension)))
-    (with-current-buffer log-buffer
-      (goto-char (point-max))
-      (insert (format "[%s] Executing: %s\n" timestamp command))
-      (insert (format "File: %s | Extension: %s | Lines: %d\n"
-                      (or (buffer-file-name) "untitled")
-                      (or file-ext "txt")
-                      (count-lines (point-min) (point-max))))
-      (insert "--- Selected Code ---\n")
-      (insert content)
-      (insert "\n--- End Code ---\n"))
-    log-buffer))
-
-(defun codesnap--execute-and-log (temp-file log-buffer)
-  "Execute codesnap command and log the result."
-  (let ((result (call-process codesnap-binary nil log-buffer t
-                              "--from-file" temp-file
-                              "--output" "clipboard"
-                              "--has-line-number")))
-    (with-current-buffer log-buffer
-      (goto-char (point-max))
-      (insert (format "Exit code: %s\n" result))
-      (if (zerop result)
-          (insert "✓ Screenshot copied to clipboard successfully\n")
-        (insert "✗ Error occurred during screenshot generation\n"))
-      (insert "==========================================\n\n"))
-    result))
+(defcustom codesnap-watermark "CodeSnap.el"
+  "Watermark that will be used within the generated screensot."
+  :type  'string
+  :group 'codesnap)
 
 ;;;###autoload
 (defun codesnap-selection ()
@@ -86,16 +45,20 @@
   (interactive)
   (unless (use-region-p)
     (user-error "No region selected"))
-  (let* ((content (buffer-substring-no-properties (region-beginning) (region-end)))
-         (temp-file (codesnap--create-temp-file content))
-         (log-buffer (codesnap--log-execution temp-file content)))
-    (unwind-protect
-        (let ((result (codesnap--execute-and-log temp-file log-buffer)))
-          (if (zerop result)
-              (message "Code screenshot copied to clipboard")
-            (message "CodeSnap failed - check %s buffer" codesnap-log-buffer)
-            (pop-to-buffer log-buffer)))
-      (delete-file temp-file))))
+  (let* ((filename    (buffer-file-name))
+         (start-range (line-number-at-pos (region-beginning)))
+         (end-range   (line-number-at-pos (region-end)))
+         (log-buffer  (get-buffer-create codesnap-log-buffer))
+         (result      (call-process codesnap-binary nil log-buffer t
+                                    "--from-file" filename
+                                    "--output"    "clipboard"
+                                    "--range"     (format "%d:%d" start-range end-range)
+                                    "--watermark" codesnap-watermark
+                                    "--has-line-number")))
+    (if (zerop result)
+        (message "CodeSnap: screenshot copied to clipboard")
+      (message "CodeSnap: failed - check %s buffer" log-buffer)
+      (pop-to-buffer log-buffer))))
 
 ;;;###autoload
 (defun codesnap-show-log ()
